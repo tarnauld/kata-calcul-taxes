a647944554291346217106afe892ebbab7b6cebd
Trisralei <timothee.arnauld1@gmail.com>
2022-11-16 08:56:33 +0100

    :recycle: use ngFor to display tabs

--- src/app/taxes/components/tabs/tabs.component.ts
+++ src/app/taxes/components/tabs/tabs.component.ts
@@ -5,4 +5,18 @@ import { Component } from "@angular/core";
   templateUrl: "./tabs.component.html",
   styleUrls: ["./tabs.component.scss"],
 })
-export class TabsComponent {}
+export class TabsComponent {
+  tabs = [{
+    title: "Input #1",
+    child: 'Input #1'
+  }, {
+    title: "Input #2",
+    child: 'Input #2'
+  }, {
+    title: "Input #3",
+    child: 'Input #3'
+  }, {
+    title: "Custom",
+    child: 'Custom'
+  }]
+}
