c3e80a22cf0a3655a34d16637daf4cd4cbcc9ad0
Trisralei <timothee.arnauld1@gmail.com>
2022-11-16 10:48:51 +0100

    :recycle: split angular material modules

---
+++ src/app/angular-material.ts
@@ -0,0 +1,13 @@
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatTableModule } from "@angular/material/table";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+
+export const materialModules = [
+  MatTabsModule,
+  MatTableModule,
+  MatDividerModule,
+  MatButtonModule,
+  MatCheckboxModule,
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,9 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SharedModule } from "src/shared/shared.module";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatTableModule } from "@angular/material/table";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCheckboxModule } from "@angular/material/checkbox";
+import { materialModules } from "./angular-material";
 import { taxesComponents } from "./taxes";
 
-const materialModules = [MatTabsModule, MatTableModule, MatDividerModule, MatButtonModule, MatCheckboxModule];
 
 @NgModule({
   declarations: [...taxesComponents, AppComponent],
