a67f7654a72fc84f6e6efe0698b97ff85ea8f142
Trisralei <timothee.arnauld1@gmail.com>
2022-11-16 15:33:45 +0100

    :sparkles: display form inputs in the custom mode

--- src/app/angular-material.ts
+++ src/app/angular-material.ts
@@ -4,9 +4,15 @@ import { MatDividerModule } from "@angular/material/divider";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
 
 export const materialModules = [
   MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
   MatTabsModule,
   MatTableModule,
   MatDividerModule,
